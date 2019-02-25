91a83c82d3f9ec42817e309cad8d9e7c149655db
Megan Rapach <meganrapach@gmail.com>
2019-02-25 11:42:45 -0500

    First Commit

---
+++ app/catalog.py
@@ -0,0 +1,33 @@
+class Catalog():
+    items = [
+        {
+            'title': 'Adam Levine Superbowl LIII Tank Pattern',
+            'price': '10.00',
+            'imagePath': '/static/images/adamLevineTankSock.png'
+        },
+        {
+            'title': 'Official SocDoc Logo',
+            'price': '7.00',
+            'imagePath': '/static/images/officialSocDocLogoSock.png'
+        },
+        {
+            'title': 'Placeholder Sock 1',
+            'price': '9.00',
+            'imagePath': '/static/images/placeholderSock.png'
+        },
+        {
+            'title': 'Placeholder Sock 2',
+            'price': '10.00',
+            'imagePath': '/static/images/placeholderSock.png'
+        },
+        {
+            'title': 'Placeholder Sock 3',
+            'price': '6.00',
+            'imagePath': '/static/images/placeholderSock.png'
+        },
+        {
+            'title': 'Placeholder Sock 4',
+            'price': '9.00',
+            'imagePath': '/static/images/placeholderSock.png'
+        }
+    ]
---
+++ app/routes.py
@@ -0,0 +1,79 @@
+from flask import render_template, flash, redirect, url_for, request, jsonify
+from app import app
+from app.forms import LoginForm
+
+from app.catalog import Catalog
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Megan'}
+    posts = [
+        {
+            'author': {'username': 'Matt'},
+            'body': 'Beautiful day in Toms River!'
+        },
+        {
+            'author': {'username': 'Kendall'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'Post'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title = 'Sign In', form = form)
+
+@app.route('/catalog')
+def catalog():
+    catalog = Catalog()
+    return render_template('catalog.html', title='Product Catalog', catalog=catalog)
+
+@app.route('/itemDetail')
+def itemDetail():
+    catalog = Catalog()
+
+    if request.method == 'GET':
+        item = request.args.get('item', '')
+        print(type(item))
+        print(item)
+    else:
+        item = "Placeholder"
+
+    for index in range(len(catalog.items)):
+        for key in catalog.items[index]:
+            if catalog.items[index][key] == item:
+                item = catalog.items[index]
+                print(item)
+    
+    #item = request.args.get('item')
+    #itemj = jsonify(item)
+    #print(type(item))
+    #pp = pprint.PrettyPrinter(indent=4)
+    #pp.pprint(itemj)
+    return render_template('itemDetail.html', title='Product Detail', item=item, catalog=catalog)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About SocDoc')
+
+@app.route('/returnPolicy')
+def returnPolicy():
+    return render_template('returnPolicy.html', title='Return Policy')
+
+@app.route('/trackOrder')
+def trackOrder():
+    return render_template('trackOrder.html', title='Track Order')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html', title='Your Cart')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact Us')
