3f388f229c6ae9b62118af7f7a87ec089ae7b909
Megan Rapach <meganrapach@gmail.com>
2019-04-01 13:25:53 -0400

    Added API for cart and payment services

--- app/catalog.py
+++ app/catalog.py
@@ -1,38 +1,62 @@
 class Catalog():
     items = [
         {
-            'title': 'Adam Levine Superbowl LIII Tank Pattern',
+            'itemID': '1',
+            'name': 'Adam Levine Superbowl LIII Tank Pattern',
             'price': '10.00',
+            'quantity': '15',
+            'color': 'brown',
+            'size': 'S',
             'imagePath': '/static/images/adamLevineTankSock.png',
             'description': 'This is the Adam Levine Superbowl LIII Tank Pattern Sock.'
         },
         {
-            'title': 'Official SocDoc Logo',
+            'itemID': '2',
+            'name': 'Official SocDoc Logo',
             'price': '7.00',
+            'quantity': '15',
+            'color': 'yellow',
+            'size': 'S',
             'imagePath': '/static/images/officialSocDocLogoSock.png',
             'description': 'This is the Official SocDoc Logo sock.'
         },
         {
-            'title': 'BAD0 Pattern',
+            'itemID': '3',
+            'name': 'BAD0 Pattern',
             'price': '9.00',
+            'quantity': '15',
+            'color': 'black',
+            'size': 'S',
             'imagePath': '/static/images/BAD0PatternSock.png',
             'description': 'This is the BAD0 Pattern sock.'
         },
         {
-            'title': 'Justin Bieber Jersey',
+            'itemID': '4',
+            'name': 'Justin Bieber Jersey',
             'price': '10.00',
+            'quantity': '15',
+            'color': 'white',
+            'size': 'S',
             'imagePath': '/static/images/JustinBieberJerseySock.png',
             'description': 'This is the Justin Bieber Jersey sock.'
         },
         {
-            'title': 'Animal Crossing Logo',
+            'itemID': '5',
+            'name': 'Animal Crossing Logo',
             'price': '6.00',
+            'quantity': '15',
+            'color': 'green',
+            'size': 'S',
             'imagePath': '/static/images/AnimalCrossingSock.png',
             'description': 'This is the Animal Crossing Logo sock.'
         },
         {
-            'title': 'Placeholder Sock 4',
+            'itemID': '6',
+            'name': 'Placeholder Sock 4',
             'price': '9.00',
+            'quantity': '15',
+            'color': 'white',
+            'size': 'S',
             'imagePath': '/static/images/placeholderSock.png',
             'description': 'This is a placeholder sock.'
         }
